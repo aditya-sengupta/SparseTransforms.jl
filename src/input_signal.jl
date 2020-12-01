1e171ffcc8a57768058afb56efa07258668d5e4a
trsigg <trsigg@gmail.com>
2020-11-30 22:53:56 -0700

    better get_Ms_simple

--- src/input_signal.jl
+++ src/input_signal.jl
@@ -81,6 +81,6 @@ end
 
 function get_random_delta_sparse_signal(n::Int64, σ::Float64, delta::Float64, c::Int64, minpower::Float64, maxpower::Float64)
     k::Int64
-    k = ceil(c .* n .^ delta)
+    k = ceil(c * n ^ delta)
     return get_random_sparse_signal(n, k, σ, minpower, maxpower, true)
 end
